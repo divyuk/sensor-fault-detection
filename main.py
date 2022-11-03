49b86d7fade0b24a73e729b94526cc88bf6ff085
divyuk <div14kes@gmail.com>
2022-11-03 14:30:21 +0530

    Updated requiremetns.txt

--- main.py
+++ main.py
@@ -2,11 +2,15 @@
 
 from sensor.constant import training_pipeline
 from sensor.entity.config_entity import TrainingPipelineConfig,DataIngestionConfig
-
+from sensor.pipeline.training_pipeline import TrainPipeline
 
 if __name__ == "__main__":
-    # mongodb_client = MongoDBClient()
-    # print("Collection name", mongodb_client.database.list_collection_names())
+    # # mongodb_client = MongoDBClient()
+    # # print("Collection name", mongodb_client.database.list_collection_names())
     
-    training_pipeline_config = TrainingPipelineConfig()
+    # training_pipeline_config = TrainingPipelineConfig()
+    # data_ingestion_config = DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+    # print(data_ingestion_config.__dict__)
     
+    training_pipeline = TrainPipeline()
+    training_pipeline.run_pipeline()
